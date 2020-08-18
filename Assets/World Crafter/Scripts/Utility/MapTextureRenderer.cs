bfc89511eba3fc1c6ecca1646ba3510b620f1324
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-08-18 17:55:38 +0530

    more refactoring

---
+++ Assets/World Crafter/Scripts/Utility/MapTextureRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WorldCrafter.Utility
+{
+    public class MapTextureRenderer : MonoBehaviour
+    {
+        public Renderer renderer;
+
+        public void DrawNoiseMap(float[,] map)
+        {
+            int width = map.GetLength(0);
+            int height = map.GetLength(1);
+            var texture = new Texture2D(width, height);
+            var colorMap = new Color[width * height];
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    colorMap[x + y * width] = Color.Lerp(Color.black, Color.white, map[x, y]);
+                }
+            }
+            texture.SetPixels(colorMap);
+            texture.Apply();
+            renderer.sharedMaterial.mainTexture = texture;
+            renderer.transform.localScale = new Vector3(width, 1, height);
+        }
+    }
+
+}
