c8ef51a13d2d302c5f20ba72b8c318ba45de506a
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-08-18 17:44:50 +0530

    added editor

---
+++ Assets/World Crafter/Scripts/WorldCrafterEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GernerateMap))]
+public class WorldCrafterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var mapgen = target as GernerateMap;
+        if(DrawDefaultInspector())
+        {
+            if(mapgen.AutoGenerate)
+            {
+                mapgen.GenerateMap();
+            }
+        }
+        if(GUILayout.Button("Generate"))
+        {
+            mapgen.GenerateMap();
+        }
+    }
+}
