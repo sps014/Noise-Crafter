4ee7fb15d92e80defd4ad9b65366f2473af4a96a
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-08-19 22:45:09 +0530

    layers config

--- Assets/World Crafter/Scripts/EditorUtility/MapGenerator.cs
+++ Assets/World Crafter/Scripts/EditorUtility/MapGenerator.cs
@@ -10,9 +10,12 @@ namespace WorldCrafter.EditorUtility
         public int Height = 256;
         public float Scale = 0.3f;
         public bool AutoGenerate = false;
+        public int NoOfLayers=4;
+        public float Persistane = 0.1f;
+        public float Lacunarity = 0.1f;
         public void GenerateMap()
         {
-            var map = NoiseGenerator.GeneratePerlinNoise(Width, Height, Scale);
+            var map = NoiseGenerator.GeneratePerlinNoise(Width, Height, Scale,new NoiseConfig(NoOfLayers,Persistane,Lacunarity));
             var display = FindObjectOfType<MapTextureRenderer>();
             display.DrawNoiseMap(map);
         }
--- Assets/World Crafter/Scripts/NoiseGenerator.cs
+++ Assets/World Crafter/Scripts/NoiseGenerator.cs
@@ -4,23 +4,61 @@ using UnityEngine;
 
 namespace WorldCrafter
 {
+    public readonly struct NoiseConfig
+    {
+        public int NoOfLayers { get; }
+        public float Persistance { get; }
+        public float Lacunarity { get; }
+
+        public NoiseConfig(int noOfLayers, float persistance, float lacunarity)
+        {
+            NoOfLayers = noOfLayers;
+            Persistance = persistance;
+            Lacunarity = lacunarity;
+        }
+
+    }
     public static class NoiseGenerator
     {
-        public static float[,] GeneratePerlinNoise(int width,int height,float scale)
+        public static float[,] GeneratePerlinNoise(int width,int height,float scale,NoiseConfig noiseConfig)
         {
             if (scale == 0)
                 scale = 0.00001f;
 
             var noise = new float[width, height];
-
+            var maxNoiseHeight = float.MinValue;
+            var minNoiseHeight = float.MaxValue;
             for (int i = 0; i < width; i++)
             {
                 for (int j = 0; j < height; j++)
                 {
-                    var x = i / scale;
-                    var y = j / scale;
+                    float noiseHeight=0.0f;
+                    float frequency = 1;
+                    float amplitude = 1;
+                    for (int k = 0; k < noiseConfig.NoOfLayers; k++)
+                    {
+                        var x = i / scale*frequency;
+                        var y = j / scale*frequency;
+                        float perlin = Mathf.PerlinNoise(x, y) * 2 - 1;
+                        noiseHeight += perlin * amplitude;
+                        amplitude *= noiseConfig.Persistance;
+                        frequency *= noiseConfig.Lacunarity;
+                    }
+                    if (noiseHeight > maxNoiseHeight)
+                        maxNoiseHeight = noiseHeight;
+                    else if (noiseHeight < minNoiseHeight)
+                        minNoiseHeight = noiseHeight;
 
-                    noise[i, j] = Mathf.PerlinNoise(x, y);
+                    noise[i, j] = noiseHeight;
+
+                }
+            }
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    noise[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noise[x, y]);
                 }
             }
 
