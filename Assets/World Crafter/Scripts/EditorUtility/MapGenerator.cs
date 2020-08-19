80aab4592c67bf1ade4b833fb430782e73c02d53
Shivendra Pratap Singh <45932883+sps014@users.noreply.github.com>
2020-08-19 23:07:39 +0530

    ..

--- Assets/World Crafter/Scripts/EditorUtility/MapGenerator.cs
+++ Assets/World Crafter/Scripts/EditorUtility/MapGenerator.cs
@@ -11,13 +11,31 @@ namespace WorldCrafter.EditorUtility
         public float Scale = 27.0f;
         public bool AutoGenerate = false;
         public int NoOfLayers=7;
+        [Range(0,1)]
         public float Persistane = 0.5f;
         public float Lacunarity = 1.87f;
+        public Vector2 Offset;
+        public int Seed = 124;
         public void GenerateMap()
         {
-            var map = NoiseGenerator.GeneratePerlinNoise(Width, Height, Scale,new NoiseConfig(NoOfLayers,Persistane,Lacunarity));
+            var map = NoiseGenerator.GeneratePerlinNoise(Width, Height, Scale,Seed,new NoiseConfig(NoOfLayers,Persistane,Lacunarity),Offset);
             var display = FindObjectOfType<MapTextureRenderer>();
             display.DrawNoiseMap(map);
+        }
+        void OnValidate()
+        {
+            if (Width < 1)
+                Width = 1;
+            if (Height < 1)
+                Height = 1;
+            if(Lacunarity<1)
+            {
+                Lacunarity = 1;
+            }
+            if(NoOfLayers<0)
+            {
+                NoOfLayers = 1;
+            }
         }
 
     }
--- Assets/World Crafter/Scripts/NoiseGenerator.cs
+++ Assets/World Crafter/Scripts/NoiseGenerator.cs
@@ -20,14 +20,27 @@ namespace WorldCrafter
     }
     public static class NoiseGenerator
     {
-        public static float[,] GeneratePerlinNoise(int width,int height,float scale,NoiseConfig noiseConfig)
+        public static float[,] GeneratePerlinNoise(int width,int height,float scale,int seed,NoiseConfig noiseConfig,Vector2 offset)
         {
+        var RandomGen = new System.Random(seed);
             if (scale == 0)
                 scale = 0.00001f;
+            var octaveOffset = new Vector2[noiseConfig.NoOfLayers];
+            for (int i = 0; i < octaveOffset.Length; i++)
+            {
+                var offsetX = RandomGen.Next(-100000, 100000)+offset.x;
+                var offsetY = RandomGen.Next(-100000, 100000)+offset.y;
+                octaveOffset[i] = new Vector2(offsetX, offsetY);
+
+            }
 
             var noise = new float[width, height];
             var maxNoiseHeight = float.MinValue;
             var minNoiseHeight = float.MaxValue;
+
+            var halfWidth = width / 2;
+            var halfHeight = height / 2;
+
             for (int i = 0; i < width; i++)
             {
                 for (int j = 0; j < height; j++)
@@ -37,8 +50,8 @@ namespace WorldCrafter
                     float amplitude = 1;
                     for (int k = 0; k < noiseConfig.NoOfLayers; k++)
                     {
-                        var x = i / scale*frequency;
-                        var y = j / scale*frequency;
+                        var x = (i-halfWidth) / scale*frequency+octaveOffset[k].x;
+                        var y = (j-halfHeight) / scale*frequency+octaveOffset[k].y;
                         float perlin = Mathf.PerlinNoise(x, y) * 2 - 1;
                         noiseHeight += perlin * amplitude;
                         amplitude *= noiseConfig.Persistance;
